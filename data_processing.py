14f7b3f6ad8ed05fb80872545094c921f4cb3a64
anton.tarasov <Anton.Tarasov@eleks.com>
2018-12-24 21:54:58 +0200

    added dataset processing, jupyter notebook

--- data_processing.py
+++ data_processing.py
@@ -1,12 +1,16 @@
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+
 def process_data():
   """
   Function that reads the 'uk_iu-ud-train.conllu' data file and extracts
-  sentances, words, tags, features.
+  sentences, words, tags, features.
   returns : data_set list that contains: [data_entry1,data_entry2, ...]
-  where data entry contains: [sentance, [words],[tags],[features]]
-  Words list contains each word of sentance in right order.
-  Tags list contains tag for each word of the sentance in right order.
-  Features list contains features for each word of the sentance in right order.
+  where data entry contains: [sentence, [words],[tags],[features]]
+  Words list contains each word of sentence in right order.
+  Tags list contains tag for each word of the sentence in right order.
+  Features list contains features for each word of the sentence in right order.
   tags contain :
   {'CCONJ', 'SCONJ', 'NOUN', 'PUNCT', 'PRON', 'ADP', 'ADJ', 'VERB', 'ADV', 'PART', 'X', 'SYM', 'NUM', 'AUX', 'PROPN', 'DET', '_', 'INTJ'}
   where X tag is a word from foreign language.
@@ -48,27 +52,88 @@ def process_data():
   file.close()
   return data_set
 
+
 def create_language_voc(data_set):
     # Function takes data set as parameter and returns unique set of words in data set.
-    word_set = set()
+    word_set =  []
     for lst in data_set:
         for word in lst[1]:
-            word_set.add(word)
+            if word not in word_set:
+                word_set.append(word)
     return word_set
 
 
 def extract_morph_tags(data_set):
     # Function takes data set as parameter and returns unique set of tags in data set.
-    tags_set = set()
+    tags_set = []
     for lst in data_set:
         for tag in lst[2]:
-            tags_set.add(tag)
+            if tag not in tags_set:
+                tags_set.append(tag)
     return tags_set
 
 
+def extract_features(sentence, voc, tags_voc):
+    """
+    Extracts features for each word in the sentence and morph tag as a label
+    :param sentence: sentence, [words],[tags],[features]
+    :return: features_list, labels_list
+    """
+    features_list = None
+    labels_list = []
+    if not "START_SENTENCE" in tags_voc:
+        tags_voc.append("START_SENTENCE")
+    last_tag = "START_SENTENCE"
+
+    for i in range(len(sentence[1])):
+        word = sentence[1][i]
+        tag = sentence[2][i]
+
+        labels_list.append(tag)
+
+        # vector representation of word in the vocabulary
+        word_to_vec = np.zeros(len(voc))
+        word_to_vec[voc.index(word)] = 1
+
+        # vector representation of the previous word morph tag
+        last_tag_to_vec = np.zeros(len(tags_voc))
+        last_tag_to_vec[tags_voc.index(last_tag)] = 1
+        last_tag = tag
+
+        # if the word is the last in the sentence
+        is_end = np.zeros(1) if i < len(sentence[1])-1 else np.ones(1)
+
+        feature_vector = np.hstack((word_to_vec, last_tag_to_vec, is_end))
+
+        if features_list is None:
+            features_list = feature_vector
+        else:
+            features_list = np.vstack((features_list, feature_vector))
+
+    return features_list, labels_list
+
+
+def get_dataset(prepared_data, voc, tags_voc):
+    X, y = None, []
+
+    for sentence in prepared_data:
+        ftrs, lbls = extract_features(sentence, voc, tags_voc)
+        if X is None:
+            X = ftrs
+        else:
+            X = np.vstack((X,ftrs))
+        y.append(lbls)
+
+    return X, y
+
 
 if __name__ == "__main__":
-    procesed_data = process_data()
-    print(procesed_data)
-    words = create_language_voc(procesed_data)
-    tags = extract_morph_tags(procesed_data)
+    processed_data = process_data()
+    # print(processed_data)
+    words = create_language_voc(processed_data)
+    tags = extract_morph_tags(processed_data)
+    X, y = get_dataset(processed_data, words, tags)
+
+    model = LogisticRegression()
+    model.fit(X,y)
+    print(model.score(X,y))
